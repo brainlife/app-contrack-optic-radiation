418747da2f15698a06063f7ccaf1eb0aebc1d4d4
Bradley Caron <bacaron@i1.red.uits.iu.edu>
2019-11-24 23:33:00 -0500

    Reason: real file path fix

--- opticRadiationContrack.m
+++ opticRadiationContrack.m
@@ -70,9 +70,10 @@ ctrParams.executeSh = 0;
 
 % fix script for missing path to contrack c code
 scriptPath = dir(fullfile(topDir,'/tmpSubj/dtiinit/dti/fibers/conTrack/OR/*.sh'));
+contrackPath = [sprintf('%s/contrack_gen.glxa64',topDir) ' '];
 fid = fopen(fullfile(scriptPath.folder,scriptPath.name));
 text = textscan(fid,'%s','delimiter','\n');
-text{1}{2} = strcat(extractBefore(text{1}{2},' -i'),'./contrack_gen.glxa64 ',extractAfter(text{1}{2},' -i'));
+text{1}{2} = strcat(extractBefore(text{1}{2},' -i'),contrackPath,extractAfter(text{1}{2},' -i'));
 fclose(fid);
 fid = fopen(fullfile(scriptPath.folder,scriptPath.name),'w');
 fprintf(fid,'%s\n',text{:}{:});
