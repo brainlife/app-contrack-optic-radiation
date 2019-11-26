9b6d00212489ca50124df9e0722b50e4737d6223
Brad Caron <bacaron@iu.edu>
2019-11-26 03:04:18 -0500

    Reason: minor changes

--- convert.sh
+++ convert.sh
@@ -2,6 +2,10 @@
 
 eccentricity=`jq -r '.eccentricity' config.json`
 freesurfer=`jq -r '.freesurfer' config.json`
+dtiinit=`jq -r '.dtiinit' config.json`
+
+mkdir tmpSubj
+cp -R ${dtiinit} ./tmpSubj
 
 mri_convert $freesurfer/mri/lh.ribbon.mgz lh.ribbon.nii.gz
 mri_convert $freesurfer/mri/rh.ribbon.mgz rh.ribbon.nii.gz
