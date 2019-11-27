84e163c5708f094ed14bc07821f3e9b99adfb564
Brad Caron <bacaron@iu.edu>
2019-11-27 03:14:03 -0500

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -4,8 +4,8 @@ eccentricity=`jq -r '.eccentricity' config.json`
 freesurfer=`jq -r '.freesurfer' config.json`
 dtiinit=`jq -r '.dtiinit' config.json`
 
-mkdir tmpSubj
-cp -R ${dtiinit} ./tmpSubj
+mkdir tmpSubj tmpSubj/dtiinit
+cp -R ${dtiinit} ./tmpSubj/dtiinit
 
 mri_convert $freesurfer/mri/lh.ribbon.mgz lh.ribbon.nii.gz
 mri_convert $freesurfer/mri/rh.ribbon.mgz rh.ribbon.nii.gz
