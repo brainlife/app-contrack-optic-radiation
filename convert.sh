f41b773e82be629e1c2501a22bb2fb7dfd5ad57c
Brad Caron <bacaron@iu.edu>
2022-05-02 15:41:23 -0400

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -20,15 +20,15 @@ do
   else
     hem="rh"
   fi
-  mri_convert $freesurfer/mri/${hem}.ribbon.mgz ./${hem}.ribbon.nii.gz && mri_vol2vol --mov ./${hem}.ribbon.nii.gz --targ ./tmpSubj/dtiinit/dwi_aligned_trilin*.nii.gz --regheader --interp neareast --o ./${hem}.ribbon.nii.gz
+  mri_convert $freesurfer/mri/${hem}.ribbon.mgz ./tmp.${hem}.ribbon.nii.gz && mri_vol2vol --mov ./tmp.${hem}.ribbon.nii.gz --targ ./tmpSubj/dtiinit/dwi_aligned_trilin*.nii.gz --regheader --interp neareast --o ./${hem}.ribbon.nii.gz
 
   # copy over lgn and v1
   cp -R ${rois}/*${hem}.${lgn}.nii.gz ./ROI${hem}.lgn.nii.gz
   cp -R ${rois}/*${hem}.${v1}.nii.gz ./ROI${hem}.v1.nii.gz
-  [ -f ${rois}/*${hem}.exclusion.nii.gz ] && cp -R ${rois}/*${hem}.exclusion.nii.gz ./ROI${hem}.exclusion.nii.gz && mri_vol2vol --mov ./ROI${hem}.exclusion.nii.gz --targ ./tmpSubj/dtiinit/dwi_aligned_trilin*.nii.gz --regheader --interp neareast --o ./ROI${hem}.exclusion.nii.gz
+  [ -f ${rois}/*${hem}.exclusion.nii.gz ] && cp -R ${rois}/*${hem}.exclusion.nii.gz ./tmp.ROI${hem}.exclusion.nii.gz && mri_vol2vol --mov ./tmp.ROI${hem}.exclusion.nii.gz --targ ./tmpSubj/dtiinit/dwi_aligned_trilin*.nii.gz --regheader --interp neareast --o ./ROI${hem}.exclusion.nii.gz
 done
 
 # convert ribbon
 mri_convert $freesurfer/mri/ribbon.mgz ribbon.nii.gz
 
-mri_vol2vol --mov ./csf_bin.nii.gz --targ ./tmpSubj/dtiinit/dwi_aligned_trilin*.nii.gz --regheader --interp neareast --o ./csf_bin.nii.gz
+mri_vol2vol --mov ./tmp.csf_bin.nii.gz --targ ./tmpSubj/dtiinit/dwi_aligned_trilin*.nii.gz --regheader --interp neareast --o ./csf_bin.nii.gz
