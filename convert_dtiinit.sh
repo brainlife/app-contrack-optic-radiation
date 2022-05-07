9049a9ad575deaef2f95648ef4d728c628ed029e
Bradley Caron <bacaron@i15.red.uits.iu.edu>
2022-05-06 23:51:10 -0400

    Reason: small fixes

--- convert_dtiinit.sh
+++ convert_dtiinit.sh
@@ -3,7 +3,7 @@
 anat=`jq -r '.t1' config.json`
 
 # remove all scripts and derivatives generated from prep step. just needed that for pdf file
-rm -rf /tmpSubj/dtiinit/dti/fibers/conTrack/OR/* ./tmpSubj/ConTrack/OR/shellScripts/* ./tmpSubj/ConTrack/OR/logs/* ./tmpSubj/dtiinit/dti/bin/lgn* ./tmpSubj/dtiinit/ROIs/*
+rm -rf ./tmpSubj/dtiinit/dti/fibers/conTrack/OR/* ./tmpSubj/ConTrack/OR/shellScripts/* ./tmpSubj/ConTrack/OR/logs/* ./tmpSubj/dtiinit/dti/bin/lgn* ./tmpSubj/dtiinit/ROIs/*
 
 # move b0,wm,and pddDispersion to ribbon ROI space in order to avoid tesselation of ROIs during contrack
 files=(`ls ./tmpSubj/dtiinit/dti/bin/`)
